6febec1b826ce2f21412894f64472af2b3c085ff
karomitk <karoline.kleibenzettl@stud.hcw.ac.at>
2026-03-23 19:55:00 +0100

    exercise 1

--- server/server.js
+++ server/server.js
@@ -6,47 +6,47 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'files')));
 
 // Configure a 'get' endpoint for data..
-pp.get('/movies', function (req, res) {
-  res.json([
-    {
+app.get('/movies', function (req, res) {
+    res.json([
+      {
       "Title":"Spider-Man",
       "Released":"2002-05-03",
-      "Runtime": 121,
-      "Genres":["Action, Adventure, Sci-Fi"],
+      "Runtime":121,
+      "Genres":["Action","Adventure","Sci-Fi"],
       "Directors":["Sam Raimi"],
-      "Writers":["Stan Lee, Steve Ditko, David Koepp"],
-      "Actors":["Tobey Maguire, Kirsten Dunst, Willem Dafoe"],
+      "Writers":["Stan Lee","Steve Ditko","David Koepp"],
+      "Actors":["Tobey Maguire","Kirsten Dunst","Willem Dafoe"],
       "Plot":"After being bitten by a genetically-modified spider, a shy teenager gains spider-like abilities that he uses to fight injustice as a masked superhero and face a vengeful enemy.",
       "Poster":"https://m.media-amazon.com/images/M/MV5BZWM0OWVmNTEtNWVkOS00MzgyLTkyMzgtMmE2ZTZiNjY4MmFiXkEyXkFqcGc@._V1_SX300.jpg",
-      "Metascore":"73",
-      "imdbRating":"7.4"
+      "Metascore": 73,
+      "imdbRating": 7.4
     },
-    {
-      "Title": "Inception",
-      "Released": "2010-07-16",
-      "Runtime": 148,
-      "Genres": ["Action", "Adventure", "Sci-Fi"],
-      "Directors": ["Christopher Nolan"],
-      "Writers": ["Christopher Nolan"],
-      "Actors": ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"],
-      "Plot": "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea.",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMmEzNTkzNzQtYzY0MC00ZjQ2LWE2NjAtODhiZjg1OTAxYzhlXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-      "Metascore": 74,
-      "imdbRating": 8.8
-    },
-    {
-      "Title": "The Matrix",
-      "Released": "1999-03-31",
+    { "Title":"The Amazing Spider-Man",
+      "Released":"2012-07-03",
       "Runtime": 136,
       "Genres": ["Action", "Sci-Fi"],
-      "Directors": ["Lana Wachowski", "Lilly Wachowski"],
-      "Writers": ["Lana Wachowski", "Lilly Wachowski"],
-      "Actors": ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
-      "Plot": "A computer hacker learns about the true nature of his reality and his role in the war against its controllers.",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNzQzOTk3NTAtNDQ1Zi00ZjQxLTlhNjAtYzYzYzY0YzU2ZTg2XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg",
+      "Directors": ["Marc Webb"],
+      "Writers": ["James Vanderbilt", "Alvin Sargent", "Steve Kloves"],
+      "Actors": ["Andrew Garfield", "Emma Stone", "Rhys Ifans"],
+      "Plot":"After Peter Parker is bitten by a genetically altered spider, he gains newfound, spider-like powers and ventures out to save the city from the machinations of a mysterious reptilian foe.",
+      "Poster":"https://m.media-amazon.com/images/M/MV5BMjMyOTM4MDMxNV5BMl5BanBnXkFtZTcwNjIyNzExOA@@._V1_SX300.jpg",
+      "Metascore": 66,
+      "imdbRating": 6.9
+    },
+    {
+      "Title":"Spider-Man: Homecoming",
+      "Released":"2017-07-07",
+      "Runtime": 133,
+      "Genres": ["Action", "Adventure", "Sci-Fi"],
+      "Directors": ["Jon Watts"],
+      "Writers": ["Jonathan Goldstein", "John Francis Daley", "Jon Watts"],
+      "Actors": ["Tom Holland", "Michael Keaton", "Robert Downey Jr."],
+      "Plot":"Peter Parker tries to stop Adrian 'The Vulture' Toomes from selling weapons made with advanced Chitauri technology while trying to balance his life as an ordinary high school student.",
+      "Poster":"https://m.media-amazon.com/images/M/MV5BODY2MTAzOTQ4M15BMl5BanBnXkFtZTgwNzg5MTE0MjI@._V1_SX300.jpg",
       "Metascore": 73,
-      "imdbRating": 8.7
+      "imdbRating": 7.4
     }
+    
   ]);
 });
 
